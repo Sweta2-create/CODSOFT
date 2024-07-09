fd2385cf17135e98cf5ac9e490a0f0402b462577
Sweta2-create <swetakumari272000@gmail.com>
2024-07-09 21:41:43 +0530

    calculator

---
+++ script.js
@@ -0,0 +1,32 @@
+function clearEntry() {
+    const display = document.getElementById('display');
+    let currentValue = display.innerText;
+    display.innerText = currentValue.slice(0, -1); 
+    if (display.innerText === '') {
+        display.innerText = '0'; 
+    }
+}
+
+function clearAll() {
+    document.getElementById('display').innerText = '0';
+    
+}
+
+function appendToDisplay(value) {
+    const display = document.getElementById('display');
+    if (display.innerText === '0' && value !== '.') {
+        display.innerText = value;
+    } else {
+        display.innerText += value;
+    }
+}
+
+function calculateResult() {
+    const display = document.getElementById('display');
+    try {
+        display.innerText = eval(display.innerText);
+    } catch {
+        display.innerText = 'Error';
+    }
+}
+
